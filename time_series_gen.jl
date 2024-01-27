2985f4dcb923a5639bc781e203265e861557ace4
Gautham <110765914+bgautham4@users.noreply.github.com>
2024-01-27 12:08:22 +0530

    Add files via upload

--- time_series_gen.jl
+++ time_series_gen.jl
@@ -98,10 +98,12 @@ begin
 	const SLOT_DUR = 1.5 #in mins
 	const RSU_R = 0.5 #In km
 	const MAX_STEPS =  floor(Int, 60 / SLOT_DUR) 
-	const V_DISTR = Poisson(7)
+	const V_DISTR = Poisson(5)
+	const V_DISTR_2 = Poisson(7)
 	const VEL_DISTR = truncated(Normal(32,4);lower = 24, upper = 40)
-	const ALLOC_DISTR = Poisson(20)
-	const MIG_DISTR = Poisson(5)
+	const ALLOC_DISTR = Poisson(15)
+	const ALLOC_DISTR_2 = Poisson(20)
+	#const MIG_DISTR = Poisson(5)
 end
 
 # ╔═╡ 4f956cb9-85f4-48df-8749-5d60ddd64257
@@ -186,9 +188,9 @@ end
 
 # ╔═╡ b8de13fa-eafd-444b-b4cf-4ff80df4605f
 begin
-	function gen_vehs()
-		N_inject = rand(V_DISTR)
-        new_vehs = [Vehicle(i,rand(SERVICE_SET),rand(0:0.125:1),rand(VEL_DISTR), 1, 1) for i in 1:N_inject]
+	function gen_vehs(n)
+		#N_inject = rand(V_DISTR)
+        new_vehs = [Vehicle(i,rand(SERVICE_SET),rand(0:0.125:1),rand(VEL_DISTR), 1, 1) for i in 1:n]
 	end
 
 	function _update_env!(env::Env,new_vehs)
@@ -210,12 +212,12 @@ begin
         end
         #env.n_vehs = length(env.vehicles)
         env.step_no += 1 #Step through time
+		return count(to_delete) #Return the number of vehicles which have left
 	end
 
-	function reset!(env::Env)
+	function reset!(env::Env,n_vehs = 30)
 		#env = Env()
 		n_rsus = length(env.rsus)
-		n_vehs = 42
 		rsus = [RSU(i, 0.5+(i-1),[]) for i in 1:n_rsus]
 		server = EdgeServer(1,[],[])
 		vehicles = [Vehicle(i, rand(SERVICE_SET), rand(0:0.25:5),rand(VEL_DISTR),0, 1) for i in 1:n_vehs]
@@ -233,27 +235,27 @@ end
 # ╔═╡ 59d612f7-cb91-45a5-bd29-1a5c6f7ee46b
 test_env = Env()
 
+# ╔═╡ d29afdd0-b6ca-461c-aea4-17f64a3b88bc
+test_env_2 = Env()
+
 # ╔═╡ 24f565f3-695c-4012-9d4e-7f93075cf217
-# ╠═╡ disabled = true
-#=╠═╡
 begin
-	reset!(test_env)
+	reset!(test_env,25)
 	
-	info_dict = Dict{Any,Vector{Vector{Int}}}()
-	info_dict["migs"] = []
+	info_dict = Dict{Any,Vector{Int}}()
+	#info_dict["migs"] = []
 	info_dict["allocs"] = []
-	info_dict["rem_time"] = []
+	info_dict["allocs_2"] = []
+	#info_dict["rem_time"] = []
+	info_dict["rem_time_2"] = []
 	for (rsu_indx,_) in enumerate(test_env.rsus)
 		for (app_indx,_) in enumerate(SERVICE_SET)
-			info_dict[(rsu_indx,app_indx)] = [Vector{Int}() for _ in 1:100]
+			info_dict[(rsu_indx,app_indx)] = []
 		end
 	end
-	for ep in 1:100 #100 episodes
 	T_SLOTS = 40
 	
-	migs = []
-	allocs = []
-	r_time = []
+	
 	for t_slot in 1:T_SLOTS
 		for (rsu_indx,rsu) in enumerate(test_env.rsus)
 			rsu_vehs = app_aggr_rsu(rsu)
@@ -266,35 +268,34 @@ begin
 					info_dict[(rsu_indx,app_indx)] = [length(rsu_vehs[app]),]
 				end
 				"""
-				push!(info_dict[(rsu_indx,app_indx)][ep], length(rsu_vehs[app]))
+				push!(info_dict[(rsu_indx,app_indx)], length(rsu_vehs[app]))
 			end
 		end
-		new_vehs = gen_vehs()
+		new_vehs = rand(V_DISTR) |> gen_vehs
 		W = rem_time(test_env.server,1) |> length
+		W_2 = rem_time(test_env.server,2) |> length
 		"""
 		push!(info_dict["migs"],length(new_vehs))
 		push!(info_dict["allocs"],rand(ALLOC_DISTR))
 		push!(info_dict["rem_time"],W)
 		"""
-		push!(migs,length(new_vehs))
-		push!(allocs,rand(ALLOC_DISTR))
-		push!(r_time,W)
+		push!(info_dict["allocs"],rand(ALLOC_DISTR))
+		push!(info_dict["allocs_2"], rand(ALLOC_DISTR_2))
+		#push!(info_dict["rem_time"],W)
+		push!(info_dict["rem_time_2"], W_2)
 		_update_env!(test_env,new_vehs)
 	end
-	push!(info_dict["migs"],migs)
-	push!(info_dict["allocs"],allocs)
-	push!(info_dict["rem_time"],r_time)
-	end
 end
-  ╠═╡ =#
 
 # ╔═╡ 1a6eea70-0a32-4626-a38b-e1fd003597b3
+# ╠═╡ disabled = true
+#=╠═╡
 begin
-	reset!(test_env)
-	
+	reset!(test_env,25)
+	reset!(test_env_2,15)
 	info_dict = Dict{Any,Vector{Int}}()
-	info_dict["migs"] = []
-	info_dict["allocs"] = []
+	#info_dict["migs"] = []
+	info_dict["load"] = []
 	info_dict["rem_time"] = []
 	T_SLOTS = 40
 	for t_slot in 1:T_SLOTS
@@ -308,15 +309,19 @@ begin
 				end
 			end
 		end
-		new_vehs = gen_vehs()
 		W = rem_time(test_env.server,2) |> length
-		push!(info_dict["migs"],rand(MIG_DISTR))
-		push!(info_dict["allocs"],rand(ALLOC_DISTR))
+		#push!(info_dict["migs"],rand(MIG_DISTR))
+		#push!(info_dict["allocs"],rand(ALLOC_DISTR))
 		push!(info_dict["rem_time"],W)
-		
-		_update_env!(test_env,new_vehs)
+		push!(info_dict["load"], length(test_env_2.vehicles))
+		N_inj = rand(V_DISTR)
+		new_vehs = gen_vehs(N_inj)
+		N_left = _update_env!(test_env,new_vehs)
+		left_vehs = gen_vehs(N_left)
+		_update_env!(test_env_2, left_vehs)
 	end
 end
+  ╠═╡ =#
 
 # ╔═╡ 6bb3bb6c-3894-4fc8-a3cf-7f59b7bf8408
 info_dict
@@ -327,7 +332,7 @@ json_str = JSON.json(info_dict)
 # ╔═╡ fd61cad0-ec73-4d69-b02f-40d1c3fbca33
 # ╠═╡ disabled = true
 #=╠═╡
-open("../time_series/base_series_7_5_20_2.json","w") do file
+open("../time_series/base_series_5.json","w") do file
 	write(file,json_str)
 end
   ╠═╡ =#
@@ -335,7 +340,7 @@ end
 # ╔═╡ bf7144e0-4757-48a9-b32c-5d72ca79d935
 # ╠═╡ disabled = true
 #=╠═╡
-noised_series = open("../time_series/noised_series_5_5_15_1_train.json") do file
+noised_series = open("../time_series/noised_series_5_15_1_2_train.json") do file
 	json_str = read(file,String)
 	JSON.parse(json_str)
 end
@@ -358,6 +363,7 @@ x = sum((noised_series["(5, $(i))"] for i in 1:3)) .- noised_series["rem_time"]
 # ╠═1d5e4419-0970-4ff3-8554-c59c7972ca8e
 # ╠═b8de13fa-eafd-444b-b4cf-4ff80df4605f
 # ╠═59d612f7-cb91-45a5-bd29-1a5c6f7ee46b
+# ╠═d29afdd0-b6ca-461c-aea4-17f64a3b88bc
 # ╠═24f565f3-695c-4012-9d4e-7f93075cf217
 # ╠═1a6eea70-0a32-4626-a38b-e1fd003597b3
 # ╠═6bb3bb6c-3894-4fc8-a3cf-7f59b7bf8408
